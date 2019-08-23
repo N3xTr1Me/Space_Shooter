0f0d767a540a3abd25456623f2f6282844b8a51f
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-23 14:45:11 +0300

    added object and player classes

--- Spac_shooter.py
+++ Spac_shooter.py
@@ -1,10 +1,37 @@
 import tkinter as tk
 import random as rn
 
+class Obj():
+
+    def __init__(this, color):
+        this.x = this.rng(X)
+        this.y = this.rng(Y)
+        this.color = color
+        this.draw()
+
+    def rng(this, limit):
+        return rn.randint(1, limit - 1) * step
+
+    def draw(this):
+        canvas.create_rectangle((this.x, this.y),
+                               (this.x+step, this.y + step),
+                                fill = this.color)
+
+class Player(Obj):
+
+     def __init__(this, color):
+        this.x = X*step / 2 - step/2
+        this.y = Y*step - step
+        this.color = color
+        super().draw()
+
 master = tk.Tk()
 X = 12
 Y = 11
 step = 80
 canvas = tk.Canvas(master, bg = 'black', height = Y*step, width = X*step)
+
+player = Player('white')
+
 canvas.pack()
 master.mainloop()
