677fb6883b1510fdf973156d992c95fc2058fcb5
N3xTr1Me <54271030+N3xTr1Me@users.noreply.github.com>
2019-08-26 14:22:36 +0300

    added player movement and meteor gen

--- Spac_shooter.py
+++ Spac_shooter.py
@@ -13,9 +13,9 @@ class Obj():
         return rn.randint(1, limit - 1) * step
 
     def draw(this):
-        canvas.create_rectangle((this.x, this.y),
-                               (this.x+step, this.y + step),
-                                fill = this.color)
+        this.body = canvas.create_rectangle((this.x, this.y),
+                                            (this.x+step, this.y + step),
+                                             fill = this.color)
 
 class Player(Obj):
 
@@ -25,6 +25,12 @@ class Player(Obj):
         this.color = color
         super().draw()
 
+    def reDraw(this, x, y):
+        old_x, old_y = this.x, this.y
+        this.x = (this.x + x) % (step*X)
+        this.y = (this.y + y) % (step*Y)
+        canvas.move(this.body, (this.x - old_x), (this.y - old_y))
+    
 class  Hostile(Obj):
     hazards = {-1}
 
@@ -37,15 +43,34 @@ class  Hostile(Obj):
         this.color = color
         super().draw()
 
+class Meteor(Hostile):
+    def __init__(this):
+        super().__init__('brown')
+
 def enemy_gen():
     for i in range(X-2):
         p=20
         r=rn.randint(1,100)
         if (r<p):
-            enemy = newObject
+            enemy = Meteor()
         else:
             enemy = Hostile()
 
+def k_prss(event):
+    keys = {'Up', 'w', 'Down', 's', 'Left', 'a', 'Right', 'd'}
+    if event.keysym in keys:
+        keyListener(event.keysym)
+
+def keyListener(key):
+    if key == 'Up' or key == 'w':
+        player.reDraw(0, -(step))
+    elif key == 'Down' or key == 's':
+        player.reDraw(0, step)
+    elif key == 'Left' or key == 'a':
+        player.reDraw(-(step), 0)
+    elif key == 'Right' or key == 'd':
+        player.reDraw(step, 0)
+    
 master = tk.Tk()
 X = 12
 Y = 11
@@ -56,4 +81,5 @@ player = Player()
 enemy_gen()
 
 canvas.pack()
+master.bind('<KeyPress>', k_prss)
 master.mainloop()
